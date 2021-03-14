4d49a14854e1fb3991e6dbc77e0ebacab47f35c7
Volodymyr Lench <volodymyr.lench@binary-studio.com>
2021-03-14 20:53:29 +0200

    add support of jks format in java 11 (pkcs12)

--- tests/feature/toPem.spec.js
+++ tests/feature/toPem.spec.js
@@ -3,48 +3,55 @@ const path = require('path');
 const { expect } = require('chai');
 const { toPem } = require('../../');
 
-const readFile = (name) => {
-	return fs.readFileSync(path.join(__dirname, 'pem', name)).toString();
+const readFile = (name, keyPath) => {
+	return fs.readFileSync(path.join(keyPath, 'pem', name)).toString();
 };
 
-const readKey = (name) => {
-	const key = readFile(name + '.key');
+const readKey = (name, keyPath) => {
+	const key = readFile(name + '.key', keyPath);
 	const position = key.indexOf('-----BEGIN PRIVATE KEY-----');
 
 	return key.slice(position).replace(/\r\n/g, '\n');
 };
 
-const readCert = (name) => {
-	const key = readFile(name + '.pem');
+const readCert = (name, keyPath) => {
+	const key = readFile(name + '.pem', keyPath);
 	const position = key.indexOf('-----BEGIN CERTIFICATE-----');
 
 	return key.slice(position).replace(/\r\n/g, '\n');
 };
 
-describe('Keystore', () => {
-	[
-		'RSA_2048_keystore',
-		'DSA_1024_keystore',
-		'EC_256_keystore',
-		'RSA_2048_MD5withRSA_keystore',
-		'RSA_2048_SHA1withRSA_keystore',
-		'RSA_2048_SHA256withRSA_keystore',
-	].forEach(name => {
-		it('should extract certificates from ' + name, () => {
-			const cert = toPem(fs.readFileSync(path.join(__dirname, 'keystore', name + '.jks')), 'password');
+const runTests = (javaVersion, keyPath) => {
+	describe('Java ' + javaVersion, () => {
+		describe('Keystore', () => {
+			[
+				'RSA_2048_keystore',
+				'DSA_1024_keystore',
+				'EC_256_keystore',
+				'RSA_2048_MD5withRSA_keystore',
+				'RSA_2048_SHA1withRSA_keystore',
+				'RSA_2048_SHA256withRSA_keystore',
+			].forEach(name => {
+				it('should extract certificates from ' + name, () => {
+					const cert = toPem(fs.readFileSync(path.join(keyPath, 'keystore', name + '.jks')), 'password');
+			
+					expect(cert).has.key('jks-js');
+					expect(cert['jks-js'].key).to.be.eq(readKey(name, keyPath));
+					expect(cert['jks-js'].cert).to.be.eq(readCert(name, keyPath));
+				});
+			});
+		});
 	
-			expect(cert).has.key('jks-js');
-			expect(cert['jks-js'].key).to.be.eq(readKey(name));
-			expect(cert['jks-js'].cert).to.be.eq(readCert(name));
+		describe('Truststore', () => {
+			it('cert should be extracted', () => {
+				const cert = toPem(fs.readFileSync(path.join(keyPath, 'keystore', 'RSA_2048_truststore.jks')), 'password');
+	
+				expect(cert).has.key('jks-js');
+				expect(cert['jks-js'].ca).to.be.eq(readCert('RSA_2048_keystore', keyPath));
+			});
 		});
 	});
-});
+};
 
-describe('Truststore', () => {
-	it('cert should be extracted', () => {
-		const cert = toPem(fs.readFileSync(path.join(__dirname, 'keystore', 'RSA_2048_truststore.jks')), 'password');
-
-		expect(cert).has.key('jks-js');
-		expect(cert['jks-js'].ca).to.be.eq(readCert('RSA_2048_keystore'));
-	});
-});
+runTests(8, __dirname);
+runTests(11, path.join(__dirname, 'java11'));
